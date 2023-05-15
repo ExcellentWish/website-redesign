6a2d52615c1ca5731c4cef6db26f9369d8809835
Nigel Doheny <nigel.doheny93@gmail.com>
2023-05-15 16:45:40 +0000

    added template

--- emeraldcontact/settings.py
+++ emeraldcontact/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-X_FRAME_OPTIONS = "SAMEORIGIN"
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^jkjbtlb+hhb-d!$9fdg2w2b&w8ftiu$buex69_#9cscb0wn7=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+X_FRAME_OPTIONS = "SAMEORIGIN"
 ALLOWED_HOSTS = []
 
 
--- emeraldcontact/urls.py
+++ emeraldcontact/urls.py
@@ -15,16 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import handler400, handler500
+from django.urls import path, include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'), name='website_urls'),
-    
-    path('accounts/', include('allauth.urls')),
 ]
-
-handler404 = 'website.views.error_404'
-handler500 = 'website.views.error_500'
