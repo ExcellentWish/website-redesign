76bd6c47da2faa6d4cbbfd19b425f6418edd4169
Nigel Doheny <nigel.doheny93@gmail.com>
2023-06-06 10:36:50 +0000

    Deploy commit -add database

--- emeraldcontact/settings.py
+++ emeraldcontact/settings.py
@@ -88,6 +88,19 @@ DATABASES = {
 }
 
 
+if os.environ.get("DEVELOPMENT") == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
